191c8f1738be4d9b8deb5f6cc4c00ce52140bb0c
Dmitry Belyaev <dmitry.belyaev@booking.com>
2020-01-10 22:13:40 +0100

    About section

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,6 @@ import { initComponents } from './lib/component';
 import './components/TimedMenu';
 import './components/ContentSlider';
 import './components/Gallery';
+import './components/About';
 
 setTimeout(initComponents, 16);
